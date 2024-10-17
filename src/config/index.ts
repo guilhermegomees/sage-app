88785207ba27ff408fe5b4991b4018f4d9202c8d
Ryan Nicolau Lopes <61668167+ryannlopes@users.noreply.github.com>
2024-10-17 00:26:14 -0300

    Adicionando novo projeto firebase

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAouIj3GOq5jHO7iz-aT7YtnaRyQX4_LPA",
-	authDomain: "sage-42ee5.firebaseapp.com",
-	projectId: "sage-42ee5",
-	storageBucket: "sage-42ee5.appspot.com",
-	messagingSenderId: "858475999260",
-	appId: "1:858475999260:web:82b1ef05e918cbed52afd9"
-};
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const FIREBASE_AUTH = getAuth(app);
